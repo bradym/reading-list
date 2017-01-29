3d2be22884f3ee8f8202967cdaac4fdc0f541fb3
Brady Mitchell <bradym@users.noreply.github.com>
2017-01-28 18:16:55 -0800

    tagging works with new structure, add example settings.yml

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+"""
+Import saved items from reddit and ttrss into pinboard.
+"""
+
 import praw
-from pprint import pprint
+from praw.models import Submission
 import pinboard
 import yaml
 from unidecode import unidecode
-import urlparse
+from urlparse import urlparse
 import github3
 import logging
 from ttrss.client import TTRClient
@@ -19,43 +26,55 @@ class ReadingList:
 
         self.settings = yaml.safe_load(open('settings.yml'))
 
-        self.r = praw.Reddit(username=self.settings['reddit']['username'],
-                             password=self.settings['reddit']['password'],
-                             client_id=self.settings['reddit']['client_id'],
-                             client_secret=self.settings['reddit']['client_secret'],
-                             user_agent=self.settings['reddit']['user_agent'])
-
-        self.pb = pinboard.Pinboard(self.settings['pinboard']['apikey'])
-        self.gh = github3.login(self.settings['github']['username'], self.settings['github']['password'])
         self.ttrss = TTRClient(self.settings['ttrss']['url'],
                                self.settings['ttrss']['username'],
-                               self.settings['ttrss']['password']).login()
+                               self.settings['ttrss']['password'],
+                               auto_login=True)
 
-    def get_tag_by_subreddit(self, subreddit):
+        self.pb = pinboard.Pinboard(self.settings['pinboard']['apikey'])
+
+        self.gh = github3.login(self.settings['github']['username'], self.settings['github']['password'])
+
+    def get_tags_by_subreddit(self, subreddit):
+        """
+        Given a subreddit, return the tags that should be applied.
+        :param subreddit: name of the subreddit
+        :return: list
+        """
 
         sub = subreddit.lower()
 
-        if sub in self.settings['reddit']['subs']:
-            return self.settings['reddit']['subs'][sub]['tags']
-        else:
-            return []
+        tags = []
 
-    def get_tag_by_domain(self, domain):
+        for tag, tag_details in self.settings['tags'].iteritems():
+            if tag_details is not None and 'subs' in tag_details and sub in tag_details['subs']:
+                tags.append(tag)
 
-        if domain in self.settings['domain_tags']:
-            return self.settings['domain_tags'][domain]['tags']
-        else:
-            return False
+        return tags
 
-    def get_tag_by_description(self, description):
+    def get_tags_by_domain(self, domain):
+        """
+        Return the tags associated with the given domain
+        :param domain:  domain name
+        :return: list
+        """
 
-        # for string in self.settings['tags']:
-        #     pprint(string)
+        domain = domain.lower()
 
-        pprint(self.settings['tags'])
+        tags = []
 
+        for tag, tag_details in self.settings['tags'].iteritems():
+            if tag_details is not None and 'domains' in tag_details and domain in tag_details['domains']:
+                tags.append(tag)
+
+        return tags
 
     def star_github_repo(self, url):
+        """
+        Star a repository in github given the url
+        :param url:
+        :return:
+        """
 
         info = url.path.split('/')
         username = info[1]
@@ -65,21 +84,31 @@ class ReadingList:
             return self.gh.star(username, repo)
 
     def process_saved_reddit_posts(self):
+        """
+        Get saved posts from reddit and process
+        :return:
+        """
 
-        for saved in self.r.user.me().saved(limit=1000):
+        r = praw.Reddit(username=self.settings['reddit']['username'],
+                        password=self.settings['reddit']['password'],
+                        client_id=self.settings['reddit']['client_id'],
+                        client_secret=self.settings['reddit']['client_secret'],
+                        user_agent=self.settings['reddit']['user_agent'])
 
-            if isinstance(saved, praw.models.Submission):
+        for saved in r.user.me().saved(limit=1000):
 
-                tags = self.get_tag_by_subreddit(saved.subreddit.display_name)
+            if isinstance(saved, Submission):
+
+                tags = self.get_tags_by_subreddit(saved.subreddit.display_name)
 
                 if self.save_link(saved.url, saved.title, tags):
                     saved.unsave()
 
-    def ttrss_unstar(self, article_id, mode, field, data=""):
-
-        self.ttrss.update_article(article_id, mode, field, data)
-
     def process_ttrss_stars(self):
+        """
+        Create bookmarks for starred articles in TTRSS
+        :return:
+        """
 
         for headline in self.ttrss.get_headlines(feed_id=-1, limit=1000):
             logging.info('Saving url to pinboard')
@@ -87,13 +116,21 @@ class ReadingList:
 
             if self.save_link(headline.link, headline.title, tags):
                 logging.info('Link saved successfully')
-                self.ttrss_unstar(headline.id, 0, 0)
+                self.ttrss.update_article(headline.id, 0, 0)
 
     def save_link(self, url, title, tags):
+        """
+        Create bookmark in pinboard or star repo in github
+
+        :param url:
+        :param title:
+        :param tags:
+        :return:
+        """
 
         logging.info('Processing URL: {}'.format(url))
 
-        parsed_url = urlparse.urlparse(url)
+        parsed_url = urlparse(url)
 
         if parsed_url.netloc == 'github.com':
             logging.info('URL is github repo, attempting to star repo.')
@@ -102,8 +139,8 @@ class ReadingList:
                 return True
         else:
             if len(tags) == 0:
-                domain = parsed_url = urlparse.urlparse(saved.url).netloc
-                tags = self.get_tag_by_domain(domain)
+                domain = parsed_url.netloc
+                tags = self.get_tags_by_domain(domain)
 
             if len(tags) > 0:
                 logging.info('Saving url to pinboard with tags: {}'.format(','.join(tags)))
@@ -113,17 +150,8 @@ class ReadingList:
             if self.pb.posts.add(url=url, description=unidecode(title), toread=True, tags=tags):
                 return True
 
-    # def main(self):
-    #
-    #     self.process_saved_reddit_posts()
-    #     self.process_ttrss_stars()
-
-    def main(self):
-
-        for sub in self.r.user.subreddits(limit=1000):
-            print(sub.display_name)
-
 if __name__ == '__main__':
 
     rl = ReadingList()
-    rl.main()
+    rl.process_saved_reddit_posts()
+    rl.process_ttrss_stars()
